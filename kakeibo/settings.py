6fe504b6fc9f56a3ea3e56234b889663f8c6cef9
NaegiErika <54990419+NaegiErika@users.noreply.github.com>
2020-02-13 01:53:25 +0900

    make it better

--- kakeibo/settings.py
+++ kakeibo/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,9 +23,8 @@ SECRET_KEY = 'n%)pj64+poz)z%v3zha9k8ma)0uhk!_a9k9gda7for%d)%30+$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+import django_heroku
 django_heroku.settings(locals())
-
 ALLOWED_HOSTS = []
 
 
